9d1c48809c0f7b0a0bab58d5ac9e618c03930082
Administrator <gdurano.shs@gmail.com>
2024-10-20 22:34:21 +0800

    initial commit

--- public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to index.html after logout
-header("Location: index.html");
-exit();
-?>
